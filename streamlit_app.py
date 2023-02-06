0ae04cd69e0b07199d864e8d8985f11862663f53
PE2E4 <124588981+PE2E4@users.noreply.github.com>
2023-02-06 15:44:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
 
+st.title('Hello, welcome to my app !')
+
+st.write("I am really happy to discover all possibilites with the tool  :)")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+df_weather = pd.read_csv(link)
+st.write(df_weather)
+
+st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+
+viz_correlation = sns.heatmap(df_weather.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)
